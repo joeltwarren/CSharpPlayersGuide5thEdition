935c8b2ea65561ef570e2d5b3c2295026e1efdc4
joeltwarren <jtw.wolf@gmail.com>
2022-11-28 10:48:21 -0500

    Completed The Makings of a Programmer challenge

---
+++ Exercise 3/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TheMakingsOfAProgrammer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * The man, who tells you his name is Ritlin, asks you to follow him over to a few of his friends, fishing on the dock. "This one here has the makings of a Programmer!"
+             * Ritlin says. The group looks at you with eyes widening and mouths agape. Ritlin turns back to you and continues, "I haven't see nor heard tell of anybody
+             * who can wield that power in a million clock cycles of the CPU. Nobody has been able to do that since the Uncoded One showed up in these lands." He describes
+             * the shadowy and mysterious Uncoded One, an evil power that rots programs and perhaps even the world itself. The Uncoded One's presence has prevented anybody
+             * from wielding the power of programming, the only thing that might be able to stop it. Yet somehow, you have been able to grab hold of this power anyway. Ritlin's
+             * companions suddenly seem doubtful. "Can you show them what you showed me? Use some of that Programming of yours to make a program? Make something with more than
+             * one statement in it?"
+             * 
+             * Objective: 
+             * Make a program with 5 Console.WriteLine statments in it?"
+             */
+            Console.WriteLine("Here is statement one");
+            Console.WriteLine("Here is statement two");
+            Console.WriteLine("Here is statement three");
+            Console.WriteLine("Here is statement four");
+            Console.WriteLine("Here is statement five\n");
+
+            Console.WriteLine("You have just gained 50XP");
+            Console.WriteLine("Total XP: 150");
+        }
+    }
+}
