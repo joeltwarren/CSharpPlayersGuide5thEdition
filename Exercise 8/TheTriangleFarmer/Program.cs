1167b7b4ba05141d1c9f7448d82c775e73d7f4d6
joeltwarren <jtw.wolf@gmail.com>
2022-11-28 15:16:41 -0500

    spelling corrections in the Lesson notes.

--- Exercise 8/TheTriangleFarmer/Program.cs
+++ Exercise 8/TheTriangleFarmer/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             /*
-             * As you pass through the fields near Arithmetica City, you encounter P-Thag, a triangle farmer, scratching number in the dirt.
+             * As you pass through the fields near Arithmetica City, you encounter P-Thag, a triangle farmer, scratching numbers in the dirt.
              * "What is all of that writing for?" you ask.
-             * I'm just trying to calculate the are of all of my triangles. They sell by their size. The bigger they are, the more they are worth! But I have many triangles
+             * I'm just trying to calculate the area of all of my triangles. They sell by their size. The bigger they are, the more they are worth! But I have many triangles
              * on my farm, and the math gets tricky, and I sometimes make mistakes. Taking a tiny triangle to town thinking your're going to get 100 gold, only to be told it's
-             * worth three, is not fun! If only i had a program that could help me....."Suddenly, P-Thag looks at you with fresh eyes. "Wait just a moment. You have the look
+             * worth three, is not fun! If only I had a program that could help me....."Suddenly, P-Thag looks at you with fresh eyes. "Wait just a moment. You have the look
              * of a Programmer about you. Can you help me write a program that will compute the areas for me, so I can quit worrying about math mistakes and get back to tending to 
              * my triangles? The equation I'm using is this one here," he says, pointing to the formula, etched in a stone beside him: Area = Base times Height divided by 2
              * 
