bd64b18f66d91bcf29e40723aa12f90ceeedde58
joeltwarren <jtw.wolf@gmail.com>
2022-11-28 10:35:35 -0500

    Completed exercise 2 What comes next

---
+++ Exercise 2/WhatComesNext/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WhatComesNext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * The Man seems surprised that you've produced a working "Hello, World!" program. "Been a while since I saw somebody program like that around here.
+             * Do you know what  you're doing with that? Can you make it do something besides just say 'hello'?"
+             */ 
+            Console.WriteLine("Why yes sir I can!\n");
+            Console.WriteLine("You have just gained 50XP");
+            Console.WriteLine("Total XP: 100");
+        }
+    }
+}
