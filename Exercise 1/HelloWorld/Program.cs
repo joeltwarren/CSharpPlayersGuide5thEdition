6f32b895ce2a18d9e5eadf4a634c52661fdd12b3
joeltwarren <jtw.wolf@gmail.com>
2022-11-28 10:26:45 -0500

    Rebuilt Folder Structure and Completed exercise 1

---
+++ Exercise 1/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * You open your eyes and find yourself face down on the beach of a large island, the waves crashing on the shore not far off. A voice nearby calls out,
+            * "Hey, you! You're finally awake! You sit up and look around. Somehow, opening your IDE has pulled you into the Realms of C#, a strange and mysterious land
+            * where it appears that you can us C# programming to solve problems. The Man comes closer, examining you. "Are you okay? Can you speak?"
+            * Create and running a "Hello World!" program seems like a good way to respond.
+            * 
+            * Objectives:
+            * Create a new Hello World program from the C# Console Application template, targeting .NET 6.
+            * Run your program using any of three methods (A) choose Debug > Start Debugging (B) press F5 (C) push the green start button on the toolbar.
+            */
+
+            Console.WriteLine("Hello World!\n");
+
+            Console.WriteLine("Congratulations: You have earned 50 XP!");
+            Console.WriteLine("Total XP: 50");
+        }
+    }
+}
