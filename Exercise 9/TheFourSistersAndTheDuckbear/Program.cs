1e9fd669da9f9e1bc42729195e2319639aca48a3
joeltwarren <jtw.wolf@gmail.com>
2022-12-05 13:12:11 -0500

    I was instructed during exercise 19 to update one of my old programs with a new method for collecting a number. Ive updated the code and commented out the original. I replaced 3 lines of code with a single statement and if needed I could call the new method as many times as I wanted to from now on.

--- Exercise 9/TheFourSistersAndTheDuckbear/Program.cs
+++ Exercise 9/TheFourSistersAndTheDuckbear/Program.cs
@@ -16,6 +16,7 @@
              * Using / and %, compute how many eggs each sister should get and how many are left over for the duckbear.
              */
 
+            /* The following code was commented out and left in place to show my original code and my new code after implementing a new method to reflect my learning history.
             //ask user for the count.
             Console.WriteLine("How many chocolate eggs were collected today?");
 
@@ -24,7 +25,9 @@
 
             // convert to an int for math operations.
             int numberOfEggs = Convert.ToInt32(collectedEggs);
+            */
 
+            int numberOfEggs = AskForNumber("How many chocolate eggs were collected today?");
             // perform normal division knowing the program will ignore the remainder using basic division.
             int forSisters = numberOfEggs / 4;
 
@@ -34,6 +37,18 @@
             // display answers for the user.
             Console.WriteLine($"Each Sister Gets {forSisters}");
             Console.WriteLine($"The duckbear gets {forDuckbear}");
+
+            /*------------------------------------------------------ Start of Methods ------------------------------------------------------ */
+
+            /// <summary>
+            /// Asks the user for a number, converts it to an int and returns it.
+            /// </summary>
+            int AskForNumber(string text)
+            {
+                Console.Write(text + " "); // displays parmater text and adds " " a blank space
+                int number = Convert.ToInt32(Console.ReadLine()); // one liner convert using the Console.ReadLine Method
+                return number; // the method has to have a return since it is not of the type void.
+            }
 
         }
     }
